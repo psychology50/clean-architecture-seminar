c75df64b7b7442ebd163c151a57bc38804093ab1
JaeSeo <qud1251@gmail.com>
2024-09-02 11:40:43 +0900

    init

---
+++ clean-architecture-seminar/clean_architecture_seminarApp.swift
@@ -0,0 +1,20 @@
+//
+//  clean_architecture_seminarApp.swift
+//  clean-architecture-seminar
+//
+//  Created by 양재서 on 9/2/24.
+//
+
+import SwiftUI
+
+@main
+struct clean_architecture_seminarApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
---
+++ clean-architecture-seminarUITests/clean_architecture_seminarUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  clean_architecture_seminarUITestsLaunchTests.swift
+//  clean-architecture-seminarUITests
+//
+//  Created by 양재서 on 9/2/24.
+//
+
+import XCTest
+
+final class clean_architecture_seminarUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
