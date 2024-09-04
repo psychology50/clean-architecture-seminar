5070407c0b15efe6847040e1a78f69df5a76895a
heejinnn <gml0593@gmail.com>
2024-09-05 02:17:07 +0900

    feat: data 계층 구현

--- clean-architecture-seminar/Domain /UseCase/UserProfileRepository.swift
+++ clean-architecture-seminar/Domain /UseCase/UserProfileRepository.swift
@@ -2,7 +2,33 @@
 //  UserProfileRepository.swift
 //  clean-architecture-seminar
 //
-//  Created by 최희진 on 9/5/24.
+//  Created by 최희진 on 9/4/24.
 //
 
 import Foundation
+
+
+protocol UserProfileRepository {
+    func fetchUserProfile() -> UserModel
+}
+
+class DefaultUserProfileRepository: UserProfileRepository {
+    
+    func fetchUserProfile() -> UserModel {
+        return UserModel(
+            id: 1,
+            username: "user1",
+            name: "홍길동",
+            isGeneralSignUp: false,
+            passwordUpdatedAt: "2023-09-04 12:00:00",
+            profileImageUrl: "https://example.com/profile.jpg",
+            phone: "010-1234-5678",
+            profileVisibility: "PUBLIC",
+            locked: false,
+            notifySetting: NotifySettingModel(accountBookNotify: true, feedNotify: true, chatNotify: true),
+            createdAt: "2023-09-04 12:00:00",
+            oauthAccount: OAuthAccountModel(kakao: true, google: false, apple: false)
+        )
+    }
+}
+
